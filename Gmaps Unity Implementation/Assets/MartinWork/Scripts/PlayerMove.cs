849e92c8951400737c34c8923a5c92f1d3feae38
sugondesenoots <martinmangahas972005@gmail.com>
2023-12-12 20:28:47 +0800

    Check-in 2
    -Fixed JUMP
    -Adjusted gravity
    -Added better visuals
    
    Status:
    -Implementing Animations
    -Implementing Audio
    -Implementing 3rd Person Cam

--- Gmaps Unity Implementation/Assets/MartinWork/Scripts/PlayerMove.cs
+++ Gmaps Unity Implementation/Assets/MartinWork/Scripts/PlayerMove.cs
@@ -6,10 +6,10 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 5.0f;
-    public bool isOnGround = true;
+    public bool isOnGround = false;
 
     private float hInput;
-    private float vInput;   
+    private float vInput;
     private Rigidbody rb;
 
     void Start()
@@ -23,17 +23,31 @@ public class PlayerMove : MonoBehaviour
         vInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput); 
-         
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
-            isOnGround = false; 
+            // rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // isOnGround = false; 
+            // Previous jump use 
+
+            Jump();
         }
     }
+
+    private void Jump()
+    {
+        rb.velocity = Vector3.zero; // Reset velocity 
+
+        Vector3 jumpDir = transform.up; // Set the jump direction to be player's UP
+
+        rb.AddForce(jumpDir * jumpForce, ForceMode.Impulse);
+        isOnGround = false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
         }
